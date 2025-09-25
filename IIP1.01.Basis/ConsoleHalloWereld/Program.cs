a4dc9a0839c8180f95964eb1c4611077957c9e2f
SmiAlwn2 <samialwan1212@gmail.com>
2025-09-25 11:07:31 +0200

    reorganisatie

---
+++ IIP1.01.Basis/ConsoleHalloWereld/Program.cs
@@ -0,0 +1,17 @@
+ using System;
+ 
+namespace ConsoleHalloWereld
+{
+	internal class Program
+	{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hallo Wereld!");
+		Console.Write("Hoe heet je: ");
+		string naam = Console.ReadLine();
+		Console.WriteLine($"Aangename kennismaking, {naam}");
+		console.WriteLine("druk een toets om verder te gaan...");
+		console.Readkey();
+	}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Introductie in Programmeren
+
+- Academiejaar: 2025-2026
+- Opleiding: Systeem Netwerk Beheer
+- Klasgroep: B BK Gr SN 1SND16
+- Naam: Sami Alwan
+
